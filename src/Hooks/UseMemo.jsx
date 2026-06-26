a47f75e01bdc4d7f42530cdc572ab77bdb4677b7
Tejraj0319 <rajeshpatil0319@gmail.com>
2026-06-26 19:51:48 +0530

    remove unused components and associated styles

--- src/Hooks/UseMemo.jsx
+++ src/Hooks/UseMemo.jsx
@@ -16,24 +16,22 @@ const UseMemo = () => {
     return count * 2;
   }, [count]);
 
-  const tripleCount = useMemo(()=>{
-    console.log("Calculating triple...");
-    return count * 3;
-  },[count])
-
   return (
     <div>
       <h2>Count: {count}</h2>
       <h3>Double: {doubleCount}</h3>
-      <h4>Triple: {tripleCount}</h4>
-
 
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <button onClick={() => setCount(0)}>Reset</button>
 
       <br />
       <br />
-      <input type="text" placeholder="Type something..." value={text} onChange={(e) => setText(e.target.value)}/>
+      <input
+        type="text"
+        placeholder="Type something..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
       <p>You typed: {text}</p>
     </div>
   );
